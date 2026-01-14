d8f9403e3de4ec6d569ae37a19d4dcea87b02bde
mpalmer79 <mpalm2025@gmail.com>
2026-01-14 08:56:04 -0500

    Create index.ts

---
+++ src/react/index.ts
@@ -0,0 +1,11 @@
+export { AgentProvider, useAgentContext, useHasAgentProvider } from './context';
+export type { AgentProviderConfig, AgentProviderProps } from './context';
+
+export { useAgent } from './useAgent';
+export type { UseAgentConfig, UseAgentReturn } from './useAgent';
+
+export { useChat } from './useChat';
+export type { UseChatConfig, UseChatReturn } from './useChat';
+
+export { useStreamingAgent } from './useStreamingAgent';
+export type { UseStreamingAgentConfig, UseStreamingAgentReturn } from './useStreamingAgent';
