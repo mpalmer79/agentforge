8bfd5e6d77823b95b2fbf4e0c5e8dd8e37628298
mpalmer79 <mpalm2025@gmail.com>
2026-01-15 10:13:21 -0500

    Update index.ts

--- src/react/index.ts
+++ src/react/index.ts
@@ -9,3 +9,22 @@ export type { UseChatConfig, UseChatReturn } from './useChat';
 
 export { useStreamingAgent } from './useStreamingAgent';
 export type { UseStreamingAgentConfig, UseStreamingAgentReturn } from './useStreamingAgent';
+
+// v1.1.0 - Enhanced React Components
+export {
+  ChatWindow,
+  MessageList,
+  MessageBubble,
+  ToolStatus,
+  TypingIndicator,
+  ChatErrorBoundary,
+  Avatar,
+} from './components';
+export type {
+  ChatWindowProps,
+  ChatWindowRef,
+  MessageListProps,
+  MessageBubbleProps,
+  ToolStatusProps,
+  TypingIndicatorProps,
+} from './components';
